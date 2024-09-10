3182c8ac3041c500add8d0bbe2c256faad5fb607
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-09-10 16:51:39 +0300

    Add

---
+++ Advanced/1.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _04.MatchingBrackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> indexes = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                string subexpression = string.Empty;
+                if (expression[i] == '(')
+                {
+                    indexes.Push(i);
+                }
+                else if (expression[i] == ')')
+                {
+                    int startIndex = indexes.Pop();
+                    subexpression = expression.Substring(startIndex, i - startIndex+1);
+
+                    Console.WriteLine(subexpression);
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/1.StacksAndQueues-Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.PrintEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 == 0)
+                {
+                    numbers.Enqueue(input[i]);
+                }
+            }
+
+            if (numbers.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", numbers));
+            }
+        }
+    }
+}

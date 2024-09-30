354161fd67aee3c6d667876a8fd7eb73d643758f
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-09-30 17:08:17 +0300

    Add

---
+++ Advanced/1.1.StacksАndQueues-Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.BasicStackOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            int n = int.Parse(command[0]), s = int.Parse(command[1]), x = int.Parse(command[2]);
+
+            Stack<int> numbers = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            if (s <= n)
+            {
+                for (int i = 0; i < s; i++)
+                    numbers.Pop();
+            }
+
+            if (numbers.Count > 0)
+            {
+                if (numbers.Contains(x))
+                    Console.WriteLine("true");
+                else
+                    Console.WriteLine(numbers.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+            
+        }
+    }
+}
---
+++ Advanced/1.1.StacksАndQueues-Exercise/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.BasicQueueOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                //The count of items we need to enter, the count of items we need to remove and the number we are looking for
+                int n = command[0], s = command[1], x = command[2];
+
+                //Entering the numbers
+                Queue<int> numbers = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+                //Removing the numbers
+                if (s <= n)
+                {
+                    for (int i = 0; i < s; i++)
+                        numbers.Dequeue();
+                }
+
+                //Checking if there are elements on the stack
+                if (numbers.Count > 0)
+                {
+                    //Checking if this number exists
+                    if (numbers.Contains(x))
+                        Console.WriteLine("true");
+                    else
+                        Console.WriteLine(numbers.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+
+
+            
+        }
+    }
+
+}

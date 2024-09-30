b8ee59f6aaae8e4cf00c9f61ba9ef249a634ea19
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-09-30 17:10:24 +0300

    Add

---
+++ Advanced/1.1.StacksАndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _05.FashionBoutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Stack<int> clothes = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+           int capacity = int.Parse(Console.ReadLine());
+
+           int racks = 1;
+           int sum = 0;
+           while (clothes.Count > 0)
+           {
+               int count = clothes.Pop();
+               sum += count;
+               if(sum > capacity)
+               {
+                   racks++;
+                   sum = count;
+               }
+           }
+
+           Console.WriteLine(racks);
+        }
+    }
+}
---
+++ Advanced/1.1.StacksАndQueues-Exercise/06.SongsQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
+
+          
+            while (songs.Count > 0)
+            {
+                string[] command = Console.ReadLine().Split().ToArray();
+
+                
+                switch (command[0])
+                {
+                    case "Play":
+                        songs.Dequeue();
+                        break;
+                    case "Add":
+                        string song = command[1];
+
+                        if (command.Length > 2)
+                        {
+                            for (int i = 2; i < command.Length; i++)
+                                song +=" " + command[i];
+                        }
+                       
+                        if (songs.Contains(song))
+                        {
+                            Console.WriteLine($"{song} is already contained!");
+                            break;
+                        }
+
+                        songs.Enqueue(song);
+                        break;
+                    case "Show":
+                        Console.WriteLine(string.Join(", ",songs));
+                        break;
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}

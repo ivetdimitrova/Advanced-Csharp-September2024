d2f1954fa4bc76b6082149bbbc783869133c5e30
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-09-30 17:40:10 +0300

    Add

---
+++ Advanced/3.SetsАndDictionariesAdvanced/07.ParkingLot/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _07.ParkingLot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parking = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] command = input.Split(", ").ToArray();
+
+                string operation = command[0];
+                string carNumber = command[1];
+
+                switch (operation)
+                {
+                    case "IN":
+                        parking.Add(carNumber);
+                        break;
+                    case "OUT":
+                        parking.Remove(carNumber);
+                        break;
+                }
+
+            }
+
+            if (parking.Count > 0)
+            {
+                foreach(string carNumber in parking)
+                {
+                    Console.WriteLine(carNumber);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            
+        }
+    }
+}
---
+++ Advanced/3.SetsАndDictionariesAdvanced/08.SoftUniParty/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _08.SoftUniParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> reservations = new HashSet<string>();
+            HashSet<string> vipReservations = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "PARTY")
+            {
+                if (input.Length == 8)
+                {
+                    if (input[0] >= 48 && input[0] <= 57)
+                    {
+                      
+                            vipReservations.Add(input);
+                    }
+                    else
+                    {
+                        reservations.Add(input);
+                    }
+                }
+               
+            }
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (reservations.Contains(input))
+                {
+                    reservations.Remove(input);
+                }
+                else if (vipReservations.Contains(input))
+                {
+                    vipReservations.Remove(input);
+                }
+            }
+
+            Console.WriteLine(reservations.Count + vipReservations.Count);
+            foreach (string reservation in vipReservations)
+            {
+                Console.WriteLine(reservation);
+            }
+            foreach (string reservation in reservations)
+            {
+                Console.WriteLine(reservation);
+            }
+        }
+    }
+}

2fa3da6cc9f3c6b4ab95eb2e012325e3ac4734c9
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-09-30 17:09:40 +0300

    Add

---
+++ Advanced/1.1.StacksАndQueues-Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _03.MaximumAndMinimumElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+
+           
+            Stack<int> numbers = new Stack<int>();
+
+           if (n >= 1 && n <= 105)
+           {
+               for (int i = 0; i < n; i++)
+               {
+                   int[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                   int numberOfCommand = command[0];
+                   if (numberOfCommand >= 1 && numberOfCommand <= 4)
+                   {
+                       switch (numberOfCommand)
+                       {
+                           case 1:
+                               int number = command[1];
+                               if (number >= 1 && number <= 109)
+                               {
+                                   numbers.Push(number);
+                               }
+                               break;
+                           case 2:
+                               numbers.Pop();
+                               break;
+                           case 3:
+                               if(numbers .Count > 0)
+                               Console.WriteLine(numbers.Max());
+                               break;
+                           case 4:
+                               if (numbers.Count > 0)
+                                    Console.WriteLine(numbers.Min());
+                               break;
+                       }
+                   }
+
+                }
+            }
+           Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ Advanced/1.1.StacksАndQueues-Exercise/04.FastFood/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _04.FastFood
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = int.Parse(Console.ReadLine());
+
+            Queue<int> orders = new Queue<int>(Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count > 0)
+            {
+                int futureQuantity = quantity - orders.Peek();
+                if (futureQuantity < 0 )
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    break;
+                }
+
+                quantity -= orders.Dequeue();
+            }
+
+            if (orders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}

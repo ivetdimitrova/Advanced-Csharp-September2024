855d238b920fe695fe66a3e06a39b879f3feb020
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-09-30 17:19:44 +0300

    Add

---
+++ Advanced/2.MultidimensionalArrays/07.PascalTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _07.PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+
+            long[][] jaggedArray = new long[numberOfRows][];
+
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                jaggedArray[i] = new long[i+1];
+
+                jaggedArray[i][0] = 1;
+                jaggedArray[i][jaggedArray[i].Length - 1] = 1;
+
+                if (i > 1)
+                {
+                    for (int j = 1; j < jaggedArray[i].Length-1; j++)
+                    {
+                        jaggedArray[i][j] = jaggedArray[i - 1][j] + jaggedArray[i - 1][j - 1];
+                    }
+                }
+            }
+
+
+            foreach (long[] row in jaggedArray)
+            {
+                Console.WriteLine(string.Join(" ",row));
+            }
+        }
+    }
+}

7e2374c946c9c83cc31cfc1abe711e8720dd7729
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-10-05 23:47:26 +0300

    Add

---
+++ Advanced/5.1.FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Predicate<string> isValid = name => name.Length <= length;
+
+            Action<string[], Predicate<string>> printValidName = (names, predicate) =>
+            {
+                foreach (string name in names)
+                {
+                    if (predicate(name))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            };
+
+            printValidName(names, isValid);
+        }
+    }
+}

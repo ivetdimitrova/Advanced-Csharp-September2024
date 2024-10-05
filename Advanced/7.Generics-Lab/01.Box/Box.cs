0e738460595cd17b3bdec094b7cded362eee5919
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-10-06 00:06:35 +0300

    Add

---
+++ Advanced/7.Generics-Lab/01.Box/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        public Stack<T> elements;
+
+        public int Count { get; private set; }
+        public Box()
+        {
+            this.elements = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            elements.Push(element);
+            Count++;
+        }
+
+        public T Remove()
+        {
+            Count--;
+            return elements.Pop();
+        }
+    }
+}
---
+++ Advanced/7.Generics-Lab/02.ArrayCreator/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+
+            int[] integers = ArrayCreator.Create(10, 33);
+
+            Console.WriteLine(string.Join(" ", strings));
+            Console.WriteLine(string.Join(" ", integers));
+        }
+    }
+}

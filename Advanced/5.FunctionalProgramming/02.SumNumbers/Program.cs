4ccb7075470d24c48bdb9eacfd536b1cbf0801ce
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-10-05 17:55:11 +0300

    Add

---
+++ Advanced/5.FunctionalProgramming/02.SumNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02.SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ Advanced/5.FunctionalProgramming/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _03.CountUppercaseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> checker = w => w[0] == w.ToUpper()[0];
+
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => checker(w))
+                .ToArray();
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}

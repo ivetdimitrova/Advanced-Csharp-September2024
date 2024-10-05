f0a6d19b67eac15769ea885146ea9a28c457a283
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-10-06 00:07:19 +0300

    Add

---
+++ Advanced/7.Generics-Lab/03.GenericScale/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>(7, 3);
+
+
+            Console.WriteLine(equalityScale.AreEqual());
+        }
+    }
+}

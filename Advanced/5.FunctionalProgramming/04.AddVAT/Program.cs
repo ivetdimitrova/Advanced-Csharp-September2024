336207c6d9d4c7f265e22ddbc8122a065319ab61
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-10-05 17:56:26 +0300

    Add

---
+++ Advanced/5.FunctionalProgramming/04.AddVAT/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Diagnostics;
+
+namespace _04.AddVAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine().Split(", ").Select(double.Parse).ToArray();
+
+            double[] pricesWithVAT = AddVAT(prices);
+
+            foreach (double price in pricesWithVAT)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+
+        public static double[] AddVAT(double[] prices)
+        {
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                double increase = prices[i] * 0.2;
+                prices[i] = prices[i] + increase;
+            }
+
+            return prices;
+        }
+    }
+}
---
+++ Advanced/5.FunctionalProgramming/05.FilterByAge/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace _05.FilterByAge
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] data = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                Person person = new Person(data[0], int.Parse(data[1]));
+
+                people.Add(person);
+            }
+
+            string condition = Console.ReadLine();
+            int ageThreshold = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            Func<Person, bool> filter = CreateFilter(condition, ageThreshold);
+            Action<Person> printer = CreatePrinter(format);
+
+            PrintPeople(people, filter, printer);
+        }
+
+        private static void PrintPeople(List<Person> people, Func<Person, bool> filter, Action<Person> printer)
+        {
+            foreach (Person person in people)
+            {
+                if (filter(person))
+                {
+                    printer(person);
+                }
+            }
+        }
+
+        public static Func<Person, bool> CreateFilter(string condition, int ageThreshold)
+        {
+            switch (condition)
+            {
+                case "younger":
+                    return x => x.Age < ageThreshold;
+                case "older":
+                    return x => x.Age >= ageThreshold;
+                    default:
+                    return x => false;
+            }
+            
+        }
+
+        public static Action<Person> CreatePrinter(string format)
+        {
+            switch (format)
+            {
+                case "name":
+                    return person => Console.WriteLine($"{person.Name}");
+                case "age":
+                    return person => Console.WriteLine($"{person.Age}");
+                case "name age":
+                    return person => Console.WriteLine($"{person.Name} - {person.Age}");
+                default:
+                    return null;
+            }
+        }
+    }
+}

a9ebc4afd27455779cee7430da4a8d30b4261d8a
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-10-05 18:02:07 +0300

    Add

---
+++ Advanced/5.1.FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> print = name => Console.WriteLine(name);
+
+            foreach (string name in names)
+            {
+                print(name);
+            }
+        }
+    }
+}
---
+++ Advanced/5.1.FunctionalProgramming-Exercise/03.CustomMinFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _03.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int minValue = int.MaxValue;
+            Func<int, int, int> minFunc = (x, y) =>
+            {
+                if (x < y) return x;
+                else return y;
+            };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                minValue = minFunc(minValue, numbers[i]);
+            }
+
+            Console.WriteLine(minValue);
+        }
+    }
+}

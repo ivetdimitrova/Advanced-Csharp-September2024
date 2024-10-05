d1476ea91b538a578709ed305e64f235051472fa
ivetdimitrova <159651783+ivetdimitrova@users.noreply.github.com>
2024-10-05 23:49:17 +0300

    Add

---
+++ Advanced/5.1.FunctionalProgramming-Exercise/11.TriFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, int, bool> isValid = (word, min) => word.Sum(y => y) >= min;
+            Func<string[], int, Func<string, int, bool>, string?> find = (all, min, predicate) => all.FirstOrDefault(x => predicate(x, min));
+
+            string? result = find(names, n, isValid);
+            if (result is not null) Console.WriteLine(result);
+        }
+    }
+}
